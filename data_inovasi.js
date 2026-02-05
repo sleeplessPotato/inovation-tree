a92db083031d33cc17e58944f18e46dda89938af
Sleepy Potato <pottatosleepy@gmail.com>
2026-02-05 14:12:59 +0700

    first commit

---
+++ data_inovasi.js
@@ -0,0 +1,66 @@
+var DATA_INOVASI = {
+	"TRACE-S": {
+		judul: "TRACE-S",
+		deskripsi:
+			"<p><strong>TRACE-S</strong> Pemanfaatan spreadsheet untuk mengumpulkan dan mengelola data sampel setiap bulannya mencakup semua komoditas menjadi satu sistem. Penggunaan TRACE-S memungkinkan petugas untuk menghimpun data secara lengkap serta melacak data spesifik berdasarkan data-data sampel yang telah diinput. Selain itu, TRACE-S akan memunculkan ringkasan data secara otomatis berdasarkan berbagai kategori seperti jenis komoditas, waktu sampling, dan lainnya. Untuk itu, kehadiran TRACE-S dapat membantu mempercepat pencarian data sampel serta rekapitulasi data sampel sebagai dasar penentuan sampel yang akan dipilih pada waktu sampling selanjutnya. </p>",
+		images: ["assets/placeholder/no_foto.png"],
+	},
+	"MALIK PERKARA": {
+		judul: "MALIK PERKARA",
+		deskripsi:
+			"<p>Penggunaan Mailing Mindik memberikan kontribusi signifikan dalam meningkatkan efektivitas dan efisiensi proses penyusunan berkas perkara oleh penyidik. Melalui fitur ini, seluruh dokumen administrasi penyidikan dikelola dengan menggabungkan satu dokumen template dengan data dari sumber lain, seperti daftar nama dan alamat pada spreadsheet, sehingga dapat menghasilkan sejumlah dokumen individual yang dipersonalisasi secara otomatis sehingga meminimalisir risiko keterlambatan maupun kesalahan dalam penyusunan dokumen</p>",
+		images: ["assets/placeholder/no_foto.png"],
+	},
+	MERDEKA: {
+		judul: "Merdeka ( Membangun Energi, Rukun, Dinamis dan Kreatif ASN )",
+		deskripsi:
+			"<p>Kegiatan ini menjadi sarana untuk memperkuat nilai-nilai kebanggaan terhadap bangsa sekaligus menumbuhkan motivasi dalam bekerja dengan penuh integritas dan dedikasi. Sebagai bagian dari instansi pemerintah yang mengemban tugas penting dalam menjamin keamanan obat dan makanan, Loka POM di Kota Lubuk Linggau memiliki peran strategis dalam menanamkan nilai-nilai tersebut kepada seluruh pegawai.</p>",
+		images: ["assets/placeholder/no_foto.png"],
+	},
+	GERLIS: {
+		judul: "GERLIS ( Gerakan Loka Linggau Sehat dan Sinergis )",
+		deskripsi:
+			"<p>GERLIS merupakan bentuk kegiatan olahraga bersama (Bulu tangkis, Gym) yang dilaksanakan oleh pegawai Loka POM di Kota Lubuk Linggau. Gerlis ini merupakan upaya mewujudkan sumber daya manusia yang sehat, produktif dan memelihara kesehatan fisik dan mental.</p>",
+		images: ["assets/placeholder/no_foto.png"],
+	},
+	EOTM: {
+		judul: "EOTM (Employee Of The Month)",
+		deskripsi:
+			"<p>Program Employee Of The Month merupakan suatu bentuk penghargaan yang diberikan kepada pegawai di Loka Pengawas Obat dan Makan di Kota Lubuk Linggau yang menunjukkan kinerja terbaik dalam periode tertentu. Tujuannya adalah untuk memotivasi, meningkatkan produktivitas, serta menumbuhkan semangat kerja dan budaya kerja yang positif di lingkungan Loka Pengawas Obat dan Makanan di Kota Lubuk Linggau.</p>",
+		images: ["assets/placeholder/no_foto.png"],
+	},
+	"SIAP CAPA": {
+		judul: "SIAP CAPA (Sistem Informasi dan Aksi Perbaikan CAPA)",
+		deskripsi:
+			"<p>Tagline “SIAP tanggapi temuan, pastikan mutu terjaga.” Dalam pelaksanaan pengawasan, sering kali ditemukan ketidaksesuaian yang memerlukan tindak lanjut berupa Corrective Action dan Preventive Action (CAPA), namun dalam pelaksanaanya banyak pelaku usaha yang masih mengalami kesulitan dalam mengidentifikasi akar penyebab temuan, menyusun rencana CAPA yang efektif, menindaklanjuti sesuai standar BPOM, melaporkan hasil perbaikannya dengan benar dan kesulitan konsultasi karena jarak sarana yang jauh dari kantor Loka POM Lubuk Linggau. Inovasi SIAP CAPA ini merupakan kegiatan yang menunjukkan semangat proaktif, edukatif, dan responsif terhadap kebutuhan pelaku usaha yang dilaksanakan oleh Petugas di Fungsi Pemeriksaan dan Sertifikasi. Pelaksanaan kegiatan yaitu layanan konsultasi langsung dengan petugas yang membantu pelaku usaha memahami, menyusun, dan menindaklanjuti Form CAPA secara cepat, tepat, dan tuntas secara daring melalui chat aplikasi Whatsapp yang dapat memperluas jangkauan pelayanan tanpa batas wilayah dan mengoptimalkan penggunaan teknologi dalam pelayanan publik. Kegiatan ini mendorong agar pelaku usaha untuk SIAP (Sigap, Inisiatif, Akurat, Proaktif) dalam menindaklanjuti temuan.</p>",
+		images: ["assets/placeholder/no_foto.png"],
+	},
+	SERIBU: {
+		judul: "SERIBU (Service Rinci Buka Informasi Registrasi Produk)",
+		deskripsi:
+			"<p>Tagline “SERIBU Informasi, Satu Portal BPOM” SERIBU merupakan Inovasi digital berupa subsite layanan informasi registrasi produk. Subsite ini digunakan untuk mempermudah petugas dalam memberikan informasi seputar registrasi produk di Badan POM dan juga mempermudah pelaku usaha dalam mendapatkan informasi mengenai registrasi produk di Badan POM. Pada subsite tersusun informasi mengai tata cara, persyaratan serta contoh dokumen dalam proses pengajuan izin di Badan POM. Subsite ini harapannya dapat mempermudah petugas dalam menyampaikan informasi dimana informasi sudah terkumpul pada satu sumber selain itu subsite juga dapat digunakan sebagai alat peraga petugas dalam menyampaikan informasi sehingga mempermudah bagi petugas untuk memberikan informasi kepada pelaku usaha. Nama SERIBU, yang merupakan singkatan dari Service Rinci Buka Informasi Registrasi Produk, menggambarkan makna “seribu informasi dalam satu sumber” yang mencerminkan semangat keterbukaan informasi publik, efisiensi pelayanan, dan kemudahan akses bagi seluruh pengguna.</p>",
+		images: ["assets/placeholder/no_foto.png"],
+	},
+	DARLING: {
+		judul: "DARLING (Edukasi Drive Thru Keliling)",
+		deskripsi:
+			"<p>DARLING (Drive Thru Edukasi Keliling) Kegiatan ini merupakan bentuk kegiatan KIE (Komunikasi, Informasi dan Edukasi) dengan menggunakan Mobil Laboratorium Keliling. Kegiatan dilakukan dengan memberikan edukasi kepada masyarakat terkait obat dan makanan secara mobile (keliling), dengan tujuan seluruh lapisan masyarakat dapat menerima edukasi yang sama terkait obat dan makanan. Kegiatan ini merupakan bentuk keterbukaan informasi publik terkait informasi obat dan makaitan yang dilakukan oleh Loka POM di Kota Lubuklinggau.</p>",
+		images: [
+			"assets/aktivitas/darling/DARLING_1.jpeg",
+			"assets/aktivitas/darling/DARLING_2.jpeg",
+			"assets/aktivitas/darling/DARLING_3.jpeg",
+			"assets/aktivitas/darling/DARLING_4.jpeg",
+		],
+	},
+	POIN: {
+		judul: "POIN (POjok INformasi) Obat dan Makanan",
+		deskripsi:
+			"<p>Kegiatan ini merupakan penyebaran informasi dan edukasi obat serta makanan dalam bentuk infografis sebagai upaya meningkatkan literasi masyarakat mengenai keamanan produk yang dikonsumsi. Kegiatan ini bertujuan untuk memperluas akses informasi yang akurat dan mudah dipahami oleh seluruh lapisan masyarakat, termasuk pelaku usaha dan UMKM. Penyampaian informasi dilakukan melalui platform media sosial resmi Loka POM di Kota Lubuk Linggau sehingga masyarakat dapat dengan cepat memperoleh pengetahuan mengenai cara memilih, menggunakan, dan mengawasi obat dan makanan yang aman. Kegiatan ini dilaksanakan secara berkala setiap 1 bulan sekali sebagai bentuk komitmen dalam mendukung pengawasan partisipatif dan mendorong masyarakat menjadi konsumen yang cerdas serta peduli akan keamanan produk yang beredar</p>",
+		images: [
+			"assets/aktivitas/poin/poin_1.jpeg",
+			"assets/aktivitas/poin/poin_2.jpeg",
+			"assets/aktivitas/poin/poin_3.jpeg",
+			"assets/aktivitas/poin/poin_4.jpeg",
+		],
+	},
+};
